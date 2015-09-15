1431c44cd3e968daaf598fb1af09c0a64f8d800f
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-09-15 16:59:20 +0200

    refactoring

--- 02-Tuesday/02-Spotify-Web/shared/js.js
+++ 02-Tuesday/02-Spotify-Web/shared/js.js
@@ -1,49 +1,43 @@
-$('form').on('submit', spotRequest);
+$('form').on('submit', artistRequest);
+$('body').on('click', 'a.album-search', albumRequest);
+$('body').on('click', 'button.track-search', trackRequest);
 
-function spotRequest(event) {
+function artistRequest(event) {
   event.preventDefault();
   var Searchterm = $('input.artist').val();
-  $.get('https://api.spotify.com/v1/search?q=' + Searchterm + '&type=Artist', handleRequest)
+  $.get('https://api.spotify.com/v1/search?q=' + Searchterm + '&type=Artist', handleArtistRequest)
 }
-
-function handleRequest(data) {
-  $('table').empty();
-  data.artists.items.forEach(function (i) {
-    var artistPicture = '<tr><td><img src=' + i.images[0].url + ' height="200px"></td>';
-    var artistName = '<td><a data_hook=' + i.id + ' class="album-search">' + i.name + '</a></td></tr>';
-    var tableRow = artistPicture + artistName;
-    $('table').append(tableRow);
-  });
-}
-
-$('body').on('click', 'a.album-search', albumRequest);
-
 function albumRequest(event) {
   event.preventDefault();
   var Searchterm = $(this).attr('data_hook');
-  $.get('https://api.spotify.com/v1/artists/' + Searchterm + '/albums', handleRequestAlbum)
+  $.get('https://api.spotify.com/v1/artists/' + Searchterm + '/albums', handleAlbumRequest)
 }
-
-function handleRequestAlbum(data) {
-  $('table').empty();
-  data.items.forEach(function (i) {
-    var albumPicture = '<tr><td><img src=' + i.images[0].url + ' height="200px"></td>';
-    var albumName = '<td><button data_hook=' + i.id + ' class="track-search">' + i.name + '</button></td></tr>';
-    var tableRow = albumPicture + albumName;
-    $('table').append(tableRow);
-  });
-}
-
-$('body').on('click', 'button.track-search', trackRequest);
-
 function trackRequest(event) {
   event.preventDefault();
   var Searchterm = $(this).attr('data_hook');
   console.log(Searchterm);
-  $.get('https://api.spotify.com/v1/albums/' + Searchterm + '/tracks', handleRequestTracks)
+  $.get('https://api.spotify.com/v1/albums/' + Searchterm + '/tracks', handleTrackRequest)
 }
 
-function handleRequestTracks(data) {
+function handleArtistRequest(data) {
+  $('table').empty();
+  data.artists.items.forEach(function (i) {
+    var artistPicture = '<tr><td><img src=' + i.images[0].url + ' class="cover img-thumbnail"></td>';
+    var artistName = '<td><a data_hook=' + i.id + ' class="btn btn-success album-search">' + i.name + '</a></td></tr>';
+    var tableRow = artistPicture + artistName;
+    $('table').append(tableRow);
+  });
+}
+function handleAlbumRequest(data) {
+  $('table').empty();
+  data.items.forEach(function (i) {
+    var albumPicture = '<tr><td><img src=' + i.images[0].url + ' class="cover img-thumbnail"></td>';
+    var albumName = '<td><a data_hook=' + i.id + ' class="btn btn-success track-search">' + i.name + '</a></td></tr>';
+    var tableRow = albumPicture + albumName;
+    $('table').append(tableRow);
+  });
+}
+function handleTrackRequest(data) {
   $('table').empty();
   data.items.forEach(function (i) {
     var trackName = '<tr><td><a href=' + i.preview_url + ' target=”_blank”>' + i.name + '</a></td></tr>';
