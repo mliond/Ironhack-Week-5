37609c6afb96ff15f1e220c489670f0ababb376f
Markus Leyendecker <markus.c.leyendecker@gmail.com>
2015-09-16 15:54:21 +0200

    finished refactoring!

--- 03-Wednesday/02-Geolocation/script.js
+++ 03-Wednesday/02-Geolocation/script.js
@@ -1,20 +1,33 @@
 $(document).on('ready', function() {
   if ("geolocation" in navigator) {
-    console.log('geo is available!')
+    console.log('geo is available!');
+    navigator.geolocation.getCurrentPosition(onLocation, onError, options);
   } else {
-    console.log('geo is NOT available')
+    console.log('geo is NOT available');
   };
 })
 
+var options = {
+  enableHighAccuracy: true
+};
+
 function onLocation(position) {
+  var lon = position.coords.longitude;
+  var lat = position.coords.latitude;
+  buildURL(lon, lat)
+}
+
+function onError(error) {
+  console.error(error);
+}
+
+function buildURL(lon, lat) {
   var access_token = 'pk.eyJ1IjoibWxpb25kIiwiYSI6ImNpZW1zc2h4cjAwMXpzdW01MjR1MzRsbmwifQ.9-JxBBJzfVwxUOsEGfPaGw';
   var map_id = 'mapbox.streets-satellite'
   var zoom = '20';
   var format = 'jpg90';
   var width = '640';
   var height = '640';
-  var lon = position.coords.longitude;
-  var lat = position.coords.latitude;
   var url = 'https://api.mapbox.com/v4/' + map_id + '/' + lon + ',' + lat + ',' + zoom + '/' + width + 'x' + height + '.' + format + '?access_token=' + access_token;
   appendImage(url);
 }
@@ -22,17 +35,3 @@ function onLocation(position) {
 function appendImage(url) {
   $('#map-placement').append('<img src=' + url + '>');
 }
-
-function buildURL() {
-
-}
-
-function onError(error) {
-  console.error(error);
-}
-
-var options = {
-  enableHighAccuracy: true
-};
-
-navigator.geolocation.getCurrentPosition(onLocation, onError, options);
